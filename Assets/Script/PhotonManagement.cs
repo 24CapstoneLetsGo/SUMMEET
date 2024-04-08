d5811966e42f7e566180605ee40049289372300f
zox2m <zox2mm@gmail.com>
2024-04-08 18:22:22 +0900

    수정

--- Assets/Script/PhotonManagement.cs
+++ Assets/Script/PhotonManagement.cs
@@ -40,7 +40,39 @@ public class PhotonManagement : MonoBehaviourPunCallbacks
     public override void OnJoinedLobby()
     {
         Debug.Log($"PhotonNetwork.InLobby = {PhotonNetwork.InLobby}");
-        //PhotonNetwork.JoinRandomRoom(); // 랜덤 매치메이킹 기능 제공 
+        PhotonNetwork.JoinRandomRoom(); // 랜덤 매치메이킹 기능 제공 
+    }
+
+    // 랜덤 룸 입장 실패시 호출되는 함수
+    public override void OnJoinRandomFailed(short returncode, string message)
+    {
+        Debug.Log($"JoinRandom Failed {returncode}:{message}");
+
+
+        // 룸의 속성 정의 
+        RoomOptions ro = new RoomOptions();
+        ro.MaxPlayers = 20; // 최대 접속자 수 20명
+        ro.IsOpen = true; // 룸의 오픈 여부
+        ro.IsVisible = true; // 로비에서 룸 목록에 노출 시킬 것인지 여부 
+
+        PhotonNetwork.CreateRoom("My room", ro);
+
+    }
+
+    // 룸 생성 후 콜백 함수 
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created Room");
+        Debug.Log($"Room name = {PhotonNetwork.CurrentRoom.Name}");
+    }
+
+    //룸 입장 후 콜백 함수
+    public override void OnJoinedRoom()
+    {
+        Debug.Log($"PhotonNetwork.InRoom = {PhotonNetwork.InRoom}");
+        Debug.Log($"Player count = {PhotonNetwork.CurrentRoom.PlayerCount}");
+
+
     }
 
     // Start is called before the first frame update
