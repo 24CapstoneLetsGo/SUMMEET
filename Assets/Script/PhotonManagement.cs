efbb82ee1a293291e170c0c551adf1ea0515064e
zox2m <zox2mm@gmail.com>
2024-04-30 14:05:11 +0900

    서버<->클라 움직임 동기화. 카메라는 실패

--- Assets/Script/PhotonManagement.cs
+++ Assets/Script/PhotonManagement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.UIElements;
 
 public class PhotonManagement : MonoBehaviourPunCallbacks
 {
